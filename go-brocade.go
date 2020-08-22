5165d416208faf77d9a7283ecc1b3422b6134b23
Sarah Vessels <cheshire137@gmail.com>
2020-08-22 14:43:52 -0500

    Add an SVG pattern and use it

--- go-brocade.go
+++ go-brocade.go
@@ -4,14 +4,19 @@ import (
 	"os"
 
 	svg "github.com/ajstarks/svgo"
+	"github.com/cheshire137/go-brocade/pkg/patterns"
 )
 
 func main() {
-	width := 500
-	height := 500
+	width := 2410
+	height := 2520
 	canvas := svg.New(os.Stdout)
 	canvas.Start(width, height)
-	canvas.Circle(width/2, height/2, 100)
-	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+
+	pattern := &patterns.FlowerAndStemSwirl{}
+	pattern.DefinePattern(canvas)
+
+	canvas.Circle(1205, 1260, 1205, "fill:url(#FlowerAndStemSwirl)")
+
 	canvas.End()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/cheshire137/go-brocade
 
 go 1.13
 
-require github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
+require github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb
