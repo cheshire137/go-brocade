fc07f63571418b4f50585787a3f2b122f90bae83
Sarah Vessels <cheshire137@gmail.com>
2020-08-22 21:26:31 -0500

    Take width, height, path params

--- README.md
+++ README.md
@@ -6,9 +6,37 @@
 
 Use to generate an SVG image of [brocade](https://en.wikipedia.org/wiki/Brocade) patterns.
 
-```bash
-make
-bin/go-brocade > some-svg-file.svg
+```sh
+% make
+% bin/go-brocade -w [WIDTH] -h [HEIGHT] -out [FILE.svg]
+```
+
+Example:
+
+```sh
+% make && bin/go-brocade
+```
+
+This will display usage instructions:
+
+```
+go build -o bin/go-brocade go-brocade.go
+Usage: bin/go-brocade [options]
+
+  -h int
+    	Height of SVG image to produce; defaults to 11" at 125px per inch (default 1375)
+  -out string
+    	Name of SVG file to create, e.g., my-image.svg
+  -w int
+    	Width of SVG image to produce; defaults to 8.5" at 125px per inch (default 1063)
+```
+
+For example:
+
+```sh
+% make && bin/go-brocade -out test.svg
+go build -o bin/go-brocade go-brocade.go
+Wrote test.svg
 ```
 
 Use a tool -- like [Gapplin](http://gapplin.wolfrosch.com) on macOS -- to view
--- go-brocade.go
+++ go-brocade.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	svg "github.com/ajstarks/svgo"
@@ -8,15 +10,42 @@ import (
 )
 
 func main() {
-	width := 1063  // 8.5", 125px per in
-	height := 1375 // 11", 125px per in
-	canvas := svg.New(os.Stdout)
+	var width int
+	flag.IntVar(&width, "w", 1063,
+		"Width of SVG image to produce; defaults to 8.5\" at 125px per inch")
+
+	var height int
+	flag.IntVar(&height, "h", 1375,
+		"Height of SVG image to produce; defaults to 11\" at 125px per inch")
+
+	var outPath string
+	flag.StringVar(&outPath, "out", "",
+		"Name of SVG file to create, e.g., my-image.svg")
+
+	flag.Parse()
+	if len(outPath) < 1 {
+		fmt.Printf("Usage: %s [options]\n\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0)
+		return
+	}
+
+	outFile, err := os.Create(outPath)
+	if err != nil {
+		fmt.Println("Could not create SVG file: " + err.Error())
+		os.Exit(1)
+		return
+	}
+
+	canvas := svg.New(outFile)
 	canvas.Start(width, height)
 
-	jupiter := patterns.NewJupiter()
-	jupiter.DefinePattern(width, height, canvas)
+	pattern := patterns.NewFlowerAndStemSwirl()
+	pattern.DefinePattern(width, height, canvas)
 
-	canvas.Rect(0, 0, width, height, jupiter.Style("#ff00ff"))
+	canvas.Rect(0, 0, width, height, pattern.Style("#ff00ff"))
 
 	canvas.End()
+
+	fmt.Printf("Wrote %s\n", outPath)
 }
