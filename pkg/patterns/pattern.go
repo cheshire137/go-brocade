ccb18d2126c6a722cb59ab5fbd8dc2fa29b5bc60
Sarah Vessels <cheshire137@gmail.com>
2020-08-23 13:28:38 -0500

    Add Pattern interface

---
+++ pkg/patterns/pattern.go
@@ -0,0 +1,14 @@
+package patterns
+
+import svg "github.com/ajstarks/svgo"
+
+type Pattern interface {
+	DefinePattern(width int, height int, canvas *svg.SVG)
+	Style(color string) string
+}
+
+var _ Pattern = (*FlowerAndStemSwirl)(nil)
+var _ Pattern = (*Jigsaw)(nil)
+var _ Pattern = (*Jupiter)(nil)
+var _ Pattern = (*Overcast)(nil)
+var _ Pattern = (*SwirlyStem)(nil)
